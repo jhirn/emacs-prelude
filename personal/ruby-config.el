8e1b55c7c80650dde2af5726278b77520f925f27
Joe Hirn <joseph.hirn@gmail.com>
2013-02-18 16:30:20 -0600

    new prelude, copied personal dir over

--- personal/ruby-config.el
+++ personal/ruby-config.el
@@ -1,4 +1,5 @@
 ;; Ruby
+
 (defun create-tags-rails (dir-name)
   "Create tags file."
   (interactive "DDirectory: ")
@@ -17,24 +18,6 @@
 
 
 (require 'rspec-mode)
-(setenv "ALL_TESTS" "true")
 (define-key rspec-mode-verifible-keymap (kbd "s") 'rspec-verify-single)
 (setq rspec-spec-command "rspec" rspec-use-rake-flag nil) ;rspec2
 
-;;defined here to deal with possible regression in 24.3.50
-(defun rspec-compile (a-file-or-dir &optional opts)
-  "Runs a compile for the specified file or diretory with the specified opts"
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "r") (eval `(lambda () (interactive)
-                                       (rspec-from-directory ,default-directory
-                                                             (rspec-compile ,a-file-or-dir (quote ,opts))))))
-    (global-set-key rspec-key-command-prefix map))
-
-  (if rspec-use-rvm
-      (rvm-activate-corresponding-ruby))
-  (rspec-from-project-root
-   (let ((compilation-scroll-output t))
-     (compile (mapconcat 'identity `(,(rspec-runner)
-                                     ,(rspec-runner-options opts)
-                                     ,a-file-or-dir) " ")
-              'rspec-compilation-mode))))
