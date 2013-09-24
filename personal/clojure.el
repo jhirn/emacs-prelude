cade55827c87acc2f065b1aa7aabe397afe38054
Joe Hirn <joseph.hirn@gmail.com>
2013-09-24 11:52:07 -0500

    bunch of nrepl goodness

--- personal/clojure.el
+++ personal/clojure.el
@@ -1,1 +1,9 @@
+(require 'nrepl)
 
+(setq nrepl-history-file "~/.emacs.d/nrepl-history")
+(setq nrepl-popup-stacktraces-in-repl t)
+(setq nrepl-history-file "~/.emacs.d/nrepl-history")
+
+
+(require 'nrepl-inspect)
+(define-key nrepl-mode-map (kbd "C-c C-i") 'nrepl-inspect)
