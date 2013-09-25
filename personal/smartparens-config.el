c640901c6f0e54a9cd1d5544269d3237687bd540
Joe Hirn <joseph.hirn@gmail.com>
2013-09-25 14:57:47 -0500

    smartparens tweaks, rainbow mode fixes to neopolitan-theme

--- personal/smartparens-config.el
+++ personal/smartparens-config.el
@@ -1,8 +1,14 @@
 (require 'smartparens)
 
-;(smartparens-global-strict-mode)
-(setq sp-highlight-pair-overlay nil
-      sp-highlight-wrap-overlay nil
-      sp-highlight-wrap-tag-overlay nil
+(setq sp-highlight-wrap-overlay nil
       sp-autoescape-string-quote nil
       sp-autoskip-closing-pair 'always)
+
+
+(show-smartparens-global-mode t)
+(custom-set-faces
+ '(sp-pair-overlay-face ((t (:background "grey20")))))
+
+
+;;sp-highlight-pair-overlay nil
+;;sp-highlight-wrap-tag-overlay nil
