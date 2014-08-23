af8360c1d2fd9038bdbbe789bddc06a95501106b
Joe Hirn <joseph.hirn@gmail.com>
2014-08-22 19:51:31 -0500

    Goodbye pop up dialogs.

--- personal/personal.el
+++ personal/personal.el
@@ -154,8 +154,17 @@
 (require 'window-number)
 (window-number-meta-mode 1)
 
-
 (require 'ansi-color)
+
+(defadvice yes-or-no-p (around prevent-dialog activate)
+  "Prevent yes-or-no-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+(defadvice y-or-n-p (around prevent-dialog-yorn activate)
+  "Prevent y-or-n-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+
 
 (add-hook 'compilation-filter-hook
           (lambda ()
