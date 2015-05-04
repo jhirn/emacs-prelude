06d15a152d27628539bcd43d0a3573734619df28
Joe Hirn <joseph.hirn@gmail.com>
2015-05-04 11:51:09 -0500

    Help and projectile mode. Helm window opens at bottom.

--- personal/helm.el
+++ personal/helm.el
@@ -8,6 +8,14 @@
       helm-projectile-fuzzy-match nil)
 
 (require 'helm-projectile)
+(helm-projectile-on)
 
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
+
+
+(add-to-list 'display-buffer-alist
+             `(,(rx bos "*helm" (* not-newline) "*" eos)
+               (display-buffer-in-side-window)
+               (inhibit-same-window . t)
+               (window-height . 0.4)))
