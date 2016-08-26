44356617b3ec8c53f412f5879bd0c9f1ef949fba
Joe Hirn <joseph.hirn@gmail.com>
2016-08-26 11:37:35 -0500

    add ox-markdown and freemind

--- personal/org-mode.el
+++ personal/org-mode.el
@@ -15,6 +15,19 @@
 
 (setq org-log-done t)
 (setq org-directory "~/src/org")
-(setq org-agenda-files '("~/src/org"))
+(setq org-agenda-files '("~/src/org" "~/src/org/retros"))
 (setq org-mobile-inbox-for-pull "~/src/org/flagged.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+(setq org-startup-folded 'nofold)
+(setq org-startup-indented t)
+
+(eval-after-load "org"
+  '(require 'ox-md nil t))
+
+(eval-after-load "org"
+  '(require 'ox-freemind nil t))
+
+(require 'org-gcal)
+;; (setq org-gcal-client-id ""
+;;       org-gcal-client-secret ""
+;;       org-gcal-file-alist '(("joseph.hirn@devmynd.com" .  "~/src/org/work-gcal.org")))
